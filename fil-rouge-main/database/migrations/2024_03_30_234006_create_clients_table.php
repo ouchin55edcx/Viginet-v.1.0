c99e04ab7ea0bc31e8d6fcd34468ebf1a90513d0
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-04-24 21:22:49 +0100

    add atribute status in table clients

--- fil-rouge-main/database/migrations/2024_03_30_234006_create_clients_table.php
+++ fil-rouge-main/database/migrations/2024_03_30_234006_create_clients_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('phone_number');
             $table->string('address');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
 
             $table->foreign('user_id')
