c0049b75e74714c8b4b63cda87089b4c4e6f2131
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-04-28 18:07:49 +0100

    login page and controller fixed

--- fil-rouge-main/app/Http/Controllers/Auth/LoginController.php
+++ fil-rouge-main/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,16 +15,14 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function store(Request $request)
+    public function store(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'login' => 'required|string',
-            'password' => 'required|string',
-        ]);
+        $credentials = $request->validated();
 
         $field = filter_var($credentials['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
         if (Auth::attempt([$field => $credentials['login'], 'password' => $credentials['password']])) {
+
             $user = Auth::user();
             switch ($user->role) {
                 case 'Client':
@@ -40,10 +39,7 @@ class LoginController extends Controller
                     break;
             }
         }
-
-
         return back()->withErrors([
             'login' => 'The provided credentials do not match our records.',
         ]);
-    }
-}
+    }}
